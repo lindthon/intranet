af3b1ca010f30e70052908fe326c8acbe07de268
chjoguer <cjguerrero_garcia@outlook.com>
2020-06-15 13:17:09 -0500

    styles y funciones de js para las tablas

--- Backend/adminsite/IPSP/static/scripts/table.js
+++ Backend/adminsite/IPSP/static/scripts/table.js
@@ -21,3 +21,35 @@ function myFunction() {
       }
     }
   }
+
+function confirmationMessage(){
+    var value = confirm("Seguro que deseas eliminar este elemento ?");
+    if(value){
+       document.forms['myform'].submit();
+       window.close();
+    }
+}
+
+function myInputBuzon() {
+  // Declare variables
+  console.log("Hoka")
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  table = document.getElementById('myTable');
+  console.log(table)
+  tr = table.getElementsByTagName('tr');
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName('td')[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
